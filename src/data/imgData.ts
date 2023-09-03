d3a6a207fcf26f813454afa1b0acef4645669769
Karsten Rabe <karstenrabe91@gmail.com>
2023-09-03 01:57:08 -0400

    buggy image carousel

---
+++ src/data/imgData.ts
@@ -0,0 +1,79 @@
+
+export type Category = "All" | "Forex" | "Speakers" | "Amplifiers" | "Mechanical" | '';
+
+export type Img = {
+  id: number;
+  alt: string;
+  attribution: string;
+  src: string;
+  imgurl: string
+  category: Category;
+};
+
+const img1: Img = {
+  id: 0,
+  alt: "flying",
+  attribution: "flying",
+  src: "/flying.jpg",
+  imgurl: "/flying.jpg",
+  category: '',
+};
+const img2: Img = {
+  id: 1,
+  alt: "speakers",
+  attribution: "speakers",
+  src: "/speakers.jpg",
+  imgurl: "/speakers.jpg",
+  category: "Speakers",
+};
+const img3: Img = {
+  id: 2,
+  alt: "tube amp",
+  attribution: "tube amp",
+  src: "/tube_amp.jpg",
+  imgurl: "/tube_amp.jpg",
+  category: "Amplifiers",
+};
+const img4: Img = {
+  id: 3,
+  alt: "forex",
+  attribution: "forex",
+  src: "/forex.png",
+  imgurl: "/forex.png",
+  category: "Forex",
+};
+const img5: Img = {
+  id: 4,
+  alt: "tube amp 2",
+  attribution: 'tube amp 2',
+  src: "/tube_amp_1.jpg",
+  imgurl: '/tube_amp_1.jpg',
+  category: "Amplifiers",
+};
+const img6: Img = {
+  id: 5,
+  alt: "engine",
+  attribution: "engine",
+  src: "/engine.jpg",
+  imgurl: '/engine.jpg',
+  category: "Mechanical",
+};
+const img7: Img = {
+  id: 6,
+  alt: "soft start board",
+  attribution: "soft start board",
+  src: "/soft_start_board.jpg",
+  imgurl: "/soft_start_board.jpg",
+  category: "Amplifiers",
+};
+const img8: Img = {
+  id: 7,
+  alt: "forex 2",
+  attribution: "forex 2",
+  src: "/forex_1.png",
+  imgurl: '/forex_1.png',
+  category: "Forex",
+};
+
+export const imgCols: Array<Array<Img>> = [[img1, img2], [img3, img4], [img5, img6], [img7, img8]];
+export const images: Array<Img> = [img1, img2, img3, img4, img5, img6, img7, img8];
