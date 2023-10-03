fa0ea9ff03dc1959d114317b778f1362f5554a17
Karsten Rabe <karstenrabe91@gmail.com>
2023-10-02 20:09:30 -0400

    use base64 instead of hex

--- compute-hash.js
+++ compute-hash.js
@@ -1,29 +1,20 @@
-import fs from 'fs';
+import { createReadStream, readdir, writeFileSync } from 'fs';
 import { createHash } from 'crypto';
-import path from 'path';
 
-fs.readdir('./dist/assets', function (err, files) {
-  try {
-    if (err) {
-      throw err;
-    }
-    const css_hashes = [];
-    const js_hashes = [];
-    
-    files.forEach(file => {
-      const buff = fs.readFileSync(`./dist/assets/${file}`);
-      const hash = createHash("sha256").update(buff).digest("hex");
+async function checksumFile(path) {
+  return new Promise((resolve, reject) => {
+    const hash = createHash('sha256');
+    const stream = createReadStream(path);
+    stream.on('error', err => reject(err));
+    stream.on('data', chunk => hash.update(chunk));
+    stream.on('end', () => resolve(hash.digest('base64')));
+  });
+}
 
-      if (file.slice(-3) === '.js') {
-        js_hashes.push(`'sha256-${hash}'`);
-      } else {
-        css_hashes.push(`'sha256-${hash}'`);
-      }
-    });
+function write_toml (css, js) {
+  const headerString = `"default-src https://metamindworks.com;media-src https://metamindworks.com;style-src ${css.join(' ')};img-src *;frame-src 'none';script-src https://netlify-rum.netlify.app ${js.join(' ')}"`;
 
-    const headerString = `"default-src https://metamindworks.com;media-src https://metamindworks.com;style-src ${css_hashes.join(' ')};img-src *;frame-src 'none';script-src https://netlify-rum.netlify.app ${js_hashes.join(' ')}"`;
-
-    const netlify_toml = `
+  const netlify_toml = `
 [[redirects]]
   from = "/*"
   to = "/index.html"
@@ -35,7 +26,30 @@ fs.readdir('./dist/assets', function (err, files) {
     Content-Security-Policy = ${headerString}
 
     `;
-    fs.writeFileSync('./netlify.toml', netlify_toml);
+writeFileSync('./netlify.toml', netlify_toml);
+};
+
+readdir('./dist/assets', async function (err, files) {
+  const len = files.length;
+  try {
+    if (err) {
+      throw err;
+    }
+    const css_hashes = [];
+    const js_hashes = [];
+    
+    await files.forEach(async (file, idx) => {
+      const hash = await checksumFile(`./dist/assets/${file}`);
+
+      if (file.slice(-3) === '.js') {
+        js_hashes.push(`'sha256-${hash}'`);
+      } else {
+        css_hashes.push(`'sha256-${hash}'`);
+      }
+      if (idx === len - 1) {
+        write_toml(css_hashes, js_hashes);
+      }
+    });
   } catch (err) {
     console.log(err);
   }
